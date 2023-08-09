c5fb16d91ccb6342936abc8f8827aae6b78410a9
vivek <ahirwarvivek19@gmail.com>
2023-08-09 13:21:13 +0530

    Added user avatar

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -15,7 +15,7 @@ import Cookies from 'js-cookie';
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
-
+import Avatar from 'react-avatar';
 
 const Header = () => {
     const { setAuth } = useContext(AuthContext);
@@ -124,7 +124,7 @@ const Header = () => {
                         </ul>
                     </li>
                     <li class="nav-item dropdown nav-user">
-                        <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src={avatar1} alt="" class="user-avatar-md rounded-circle" /></a>
+                        <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><Avatar  name={Cookies.get('username')} size='25' class="user-avatar-md rounded-circle" round textSizeRatio={2} /></a>
                         <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
                             <div class="nav-user-info">
                                 <h5 class="mb-0 text-white nav-user-name">{Cookies.get('username')} </h5>
